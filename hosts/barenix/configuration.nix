a2ef80b4d547bc0d288ddafd7f25ade0ea30a66b
Matthew Boyea <contact@mboyea.com>
2024-10-09 16:36:36 +0000

    updated basenix nixos version

--- hosts/barenix/configuration.nix
+++ hosts/barenix/configuration.nix
@@ -45,6 +45,7 @@
   #
   # For more information, see `man configuration.nix` or
   # https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion
-  system.stateVersion = "23.11";
+
+  system.stateVersion = "24.05"; # Did you read the comment?
 }
 
